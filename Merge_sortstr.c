c4365807e7b2ba3aa2f8dcf16247bfc465bb62eb
khunkhin444 <75085890+khunkhin444@users.noreply.github.com>
2021-04-28 01:35:47 +0800

    Update Merge_sortstr.c

--- Merge_sortstr.c
+++ Merge_sortstr.c
@@ -5,7 +5,6 @@
 #define buf_size 100
 #define line_cnt 1000000
 
-char *lines[line_cnt]; //global variable
 
 void merge(char **strings, char **left, char **right, int left_cnt, int right_cnt) {
     int i,j,k;
@@ -63,29 +62,4 @@ void merge_sort(char **strings, int cnt) {
     for(i = mid; i<cnt;i++) {
         free(right[i-mid]);
     }
-}
-
-int main() {
-    FILE *fp;
-    struct  timeval start;      
-    struct  timeval end;       
-    unsigned  long time;        
-    fp = fopen("dataset2.txt","r");
-    int cnt = 0;
-    char line[buf_size+1];
-    while(fgets(line,buf_size+2,fp)) {
-        lines[cnt] = malloc(sizeof(char)*101);
-        strcpy(lines[cnt],line);
-        cnt++;
-    }
-    gettimeofday(&start,NULL);
-    int size = sizeof(lines)/sizeof(lines[0]);
-    merge_sort(lines, size);
-    gettimeofday(&end,NULL);
-    for(int i=0;i<cnt;i++) {
-        printf("%s",lines[i]);
-    }
-    fclose(fp);
-    time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;   
-    printf("Sorting performance (Merge) %ld us (equal %f sec)\n", time, time / 1000000.0);
 }
