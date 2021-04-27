fabf8d20b1d4950452b591cf8ed040384402f521
khunkhin444 <75085890+khunkhin444@users.noreply.github.com>
2021-04-28 02:16:01 +0800

    Update and rename QuickSort.c to main.c

--- num_quicksort/main.c
+++ num_quicksort/main.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include "sort.h"
 #define buf_size 1000000
-int partition(int *arr, int start, int end) {
-    int p = arr[end];
-    int partition_idx = start;
-    int i;
-    int tmp;
-    for(i=start; i<end; i++) {
-        if(arr[i]<=p) {
-            tmp = arr[i];
-            arr[i]=arr[partition_idx];
-            arr[partition_idx]=tmp;
-            partition_idx++;
-        }
-    }
-    // partition_idx--;
-    tmp = arr[end];
-    arr[end] = arr[partition_idx];
-    arr[partition_idx]=tmp;
-    return partition_idx;
-}
-void quick_sort(int *arr, int start, int end) {
-    if(start < end) {
-        int idx = partition(arr, start, end);
-        quick_sort(arr, start, idx-1);
-        quick_sort(arr, idx+1, end);
-    }
-    // else return;
-}
 int main() {
     FILE *fp;
     struct timeval start;
