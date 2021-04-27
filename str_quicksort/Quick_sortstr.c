e87794ad7c5811210ea97def4d60509e0a2adcf8
khunkhin444 <75085890+khunkhin444@users.noreply.github.com>
2021-04-28 02:35:57 +0800

    Update Quick_sortstr.c

--- str_quicksort/Quick_sortstr.c
+++ str_quicksort/Quick_sortstr.c
@@ -2,39 +2,12 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
+#include "sort.h"
 #define buf_size 100
 #define line_cnt 1000000
 
 char *lines[line_cnt];
 
-void swap_string(char **str1, char **str2) {
-    char *temp = *str1;
-    *str1 = *str2;
-    *str2 = temp;
-}
-
-int partition(char **strings, int start, int end) {
-    char *tmp = malloc(sizeof(char)*101);
-    strcpy(tmp,strings[end]);
-    int partition_idx = start;
-    int i;
-    for(i=start; i<end; i++) {
-        if(strcmp(strings[i],tmp)<=0) {
-            swap_string(&strings[i],&strings[partition_idx]);
-            partition_idx++;
-        }
-    }
-    swap_string(&strings[end],&strings[partition_idx]);
-    free(tmp);
-    return partition_idx;
-}
-void quick_sort(char **strings, int start, int end) {
-    if(start < end) {
-        int idx = partition(strings, start, end);
-        quick_sort(strings, start, idx-1);
-        quick_sort(strings, idx+1, end);
-    }
-}
 int main() {
     FILE *fp;
     struct  timeval start;      
