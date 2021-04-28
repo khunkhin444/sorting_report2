510799129e50c8da8185cdbbb546b9ef5df0d758
khunkhin444 <75085890+khunkhin444@users.noreply.github.com>
2021-04-28 22:46:22 +0800

    Create MergeSortStr.c

---
+++ str_mergesort/MergeSortStr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#define line_cnt 1000000
+int main() {
+    FILE *fp;
+    struct  timeval start;
+    struct  timeval end;       
+    unsigned  long time;
+    fp = fopen("dataset2.txt","r");
+    int i, cnt = 0;
+    char line[buf_size+1];
+    while(fgets(line,buf_size+2,fp)) {
+        lines[cnt] = malloc(sizeof(char)*101);
+        tmp[cnt] = malloc(sizeof(char)*101);
+        strcpy(lines[cnt],line);
+        cnt++;
+    }
+    int first = 0;
+    int last = cnt - 1;
+    gettimeofday(&start,NULL);
+    merge_sort(first, last, line_cnt);
+    gettimeofday(&end,NULL);
+    for(i=0;i<line_cnt;i++) {
+        printf("%s",lines[i]);
+    }
+    for(i=0;i<cnt;i++) {
+    	free(lines[i]);
+    	free(tmp[i]);
+    }
+    fclose(fp);
+    time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    printf("Sorting performance (Merge) %ld us (equal %f sec)\n", time, time / 1000000.0);
+}
