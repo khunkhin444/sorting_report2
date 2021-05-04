463cc8174dd242f1fd7d8bd786db3d8350ffee21
khunkhin444 <75085890+khunkhin444@users.noreply.github.com>
2021-05-04 22:06:14 +0800

    Update main.c

--- num_quicksort/main.c
+++ num_quicksort/main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <sys/time.h>
 #include "sort.h"
 #define buf_size 1000000
 int main() {
+    FILE *fp;
     struct timeval start;
     struct timeval end;
-    unsigned long sec;
-    int arr[buf_size];
+    unsigned long time;
+    fp = fopen("dataset1.txt","r");
+    int arr[buf_size-1];
+    char line[2048];
     int i=0;
     int tmp;
-    srand(time(NULL));
-    for(i=0;i<1000000;i++) {
-        arr[i]=rand();
+    while(fgets(line,2048,fp)) {
+        tmp = atoi(line);
+        arr[i] = tmp;
+        i++;
     }
     gettimeofday(&start,NULL);
     quick_sort(arr, 0, buf_size-1);
+    quick_sort(arr, 0, buf_size-1);
     gettimeofday(&end,NULL);
-    sec = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+    time = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
     for(i=0;i<buf_size;i++) printf("%d\n",arr[i]);
     fclose(fp);
-    printf("Sorting performance (Heap) %ld us (equal %f sec)\n",sec,sec/1000000.0);
+    printf("Sorting performance (Heap) %ld us (equal %f sec)\n",time,time/1000000.0);
 }
